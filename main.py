0f344c119518ccee886b819f76e235e5b3520612
Soubhik Sinha <91076835+S-rony@users.noreply.github.com>
2026-06-15 17:23:18 +0530

    Enhance snake game with food and scoring
    Added food and scoreboard functionality to the snake game.

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
-from turtle import Turtle, Screen
+from turtle import  Screen
+
+from dask.array import square
+
+from food import Food
+from scoreboard import Score
 import time
 from snake import Snake
 screen = Screen()
 screen.setup(width = 600, height = 600)
-screen.bgcolor("black")
+screen.bgcolor("red")
 screen.title("SNAKE GAME")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+score = Score()
+
 screen.listen()
 screen.onkey(key="Up", fun=snake.up)
 screen.onkey(key="Down", fun=snake.down)
@@ -22,6 +30,22 @@ while game_is_on:
     time.sleep(0.1)
     snake.snake_move()
 
+    #Detect collision with food.
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        score.increase_score()
+
+    #Detect collision with the Wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        score.game_over()
+        game_is_on = False
+
+    #Detect collision with the Tail
+    for square in snake.all_squares[1:]:
+        if snake.head.distance(square) < 10:
+            game_is_on = False
+            score.game_over()
 
     # all_squares[0].right(90)
 
