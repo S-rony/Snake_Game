8415ce42f75aad96fd81eb9a28f813bde72ee947
Soubhik Sinha <91076835+S-rony@users.noreply.github.com>
2026-06-15 17:24:56 +0530

    Change snake color to blue and add extend method
    Updated snake color to blue and added extend method.

--- snake.py
+++ snake.py
@@ -1,9 +1,11 @@
-import turtle
-from turtle import Turtle, Screen
+from turtle import Turtle
+
 UP = 90
 DOWN = 270
 LEFT = 180
 RIGHT = 0
+
+
 class Snake:
     def __init__(self):
         self.all_squares = []
@@ -11,13 +13,21 @@ class Snake:
         self.head = self.all_squares[0]
 
     def snake(self):
-        x = [0,-20,-40]
+        x = [0, -20, -40]
         for i in range(3):
             tim = Turtle(shape="square")
-            tim.color("white")
+            tim.color("blue")
             self.all_squares.append(tim)
             self.all_squares[i].penup()
-            self.all_squares[i].goto(x[i],0)
+            self.all_squares[i].goto(x[i], 0)
+
+    def extend(self):
+        new_square = Turtle(shape="square")
+        new_square.color("blue")
+        new_square.penup()
+        new_square.goto(self.all_squares[-1].position()) #.position() is another built-in method of the Turtle class — it
+        # comes from the same turtle module, just like .goto(), .distance(), .forward(), etc.
+        self.all_squares.append(new_square)
 
     def snake_move(self):
         for square in range(len(self.all_squares) - 1, 0, -1):
@@ -29,12 +39,15 @@ class Snake:
     def up(self):
         if self.head.heading() != DOWN:
             self.head.setheading(UP)
+
     def down(self):
         if self.head.heading() != UP:
             self.head.setheading(DOWN)
+
     def left(self):
         if self.head.heading() != RIGHT:
             self.head.setheading(LEFT)
+
     def right(self):
         if self.head.heading() != LEFT:
             self.head.setheading(RIGHT)
